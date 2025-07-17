ce1410ef33b953fe4162f9f92723da46116e2c8c
Timothee Chauvin <timothee.chauvin28@gmail.com>
2025-07-17 16:03:40 +0200

    cmt

--- src/track_llm_apis/finetune_openai.py
+++ src/track_llm_apis/finetune_openai.py
@@ -81,7 +81,12 @@ def job_exists(
     lr_multiplier: float,
     batch_size: int,
     n_epochs: int,
-) -> str | None:
+) -> tuple[bool, str | None, str | None]:
+    """
+    Check if a job with these parameters already exists.
+
+    Return a tuple of (exists, status, name of finetuned model).
+    """
     for job in existing_jobs:
         if all(
             (
@@ -92,69 +97,68 @@ def job_exists(
                 job.hyperparameters.n_epochs == n_epochs,
             )
         ):
-            return job.fine_tuned_model
-    return None
+            return True, job.status, job.fine_tuned_model
+    return False, None, None
 
 
-def main(gen_files: bool = False, upload_files: bool = False, finetune: bool = False):
-    if upload_files:
-        assert gen_files, "Must generate files before uploading"
-    if gen_files:
-        if DATASET_NAME == "lmsys-chat-1m":
-            dataset = load_lmsys_chat_1m(
-                gpt4_filter=True, redacted_filter=True, flagged_filter=True, first_turn_only=True
-            )
-        else:
-            raise NotImplementedError(f"Dataset {DATASET_NAME} not implemented")
-        indices = np.random.permutation(len(dataset))
-        dataset = dataset.select(indices)
-        for sample_size in SAMPLE_SIZES:
-            subset = dataset[:sample_size]
-            jsonl_path = generate_jsonl(subset)
-            if upload_files:
-                upload_jsonl(jsonl_path)
+def generate_files(upload: bool = False):
+    if DATASET_NAME == "lmsys-chat-1m":
+        dataset = load_lmsys_chat_1m(
+            gpt4_filter=True, redacted_filter=True, flagged_filter=True, first_turn_only=True
+        )
+    else:
+        raise NotImplementedError(f"Dataset {DATASET_NAME} not implemented")
+    indices = np.random.permutation(len(dataset))
+    dataset = dataset.select(indices)
+    for sample_size in SAMPLE_SIZES:
+        subset = dataset[:sample_size]
+        jsonl_path = generate_jsonl(subset)
+        if upload:
+            upload_jsonl(jsonl_path)
 
-    if finetune:
-        existing_finetuning_jobs = client.fine_tuning.jobs.list().data
-        for sample_size in SAMPLE_SIZES:
-            existing_files = client.files.list().data
-            for f in existing_files:
-                if f.filename == file_name(sample_size):
-                    file_id = f.id
-                    break
-            for model in MODELS:
-                for lr_multiplier in LR_MULTIPLIERS:
-                    suffix = f"lr={lr_multiplier:g}_samples={sample_size}_epochs={EPOCHS}_batch_size={BATCH_SIZE}"
-                    logger.info(f"Creating finetuning job with {model}, {suffix=}...")
-                    # Find if a job with these parameters already exists
-                    if job_name := job_exists(
-                        existing_finetuning_jobs, model, file_id, lr_multiplier, BATCH_SIZE, EPOCHS
-                    ):
-                        logger.info(f"Job already exists with name {job_name}. Skipping")
-                        continue
-                    hyperparameters = {
-                        "learning_rate_multiplier": lr_multiplier,
-                        "batch_size": BATCH_SIZE,
-                        "n_epochs": EPOCHS,
-                    }
-                    if input("Are you sure you want to send this job? (y/N)") != "y":
-                        logger.info("Skipping")
-                        continue
-                    client.fine_tuning.jobs.create(
-                        model=model,
-                        training_file=file_id,
-                        seed=Config.seed,
-                        suffix=suffix,
-                        method={
-                            "type": "supervised",
-                            "supervised": {
-                                "hyperparameters": hyperparameters,
-                            },
+
+def finetune():
+    existing_finetuning_jobs = client.fine_tuning.jobs.list().data
+    for sample_size in SAMPLE_SIZES:
+        existing_files = client.files.list().data
+        for f in existing_files:
+            if f.filename == file_name(sample_size):
+                file_id = f.id
+                break
+        for model in MODELS:
+            for lr_multiplier in LR_MULTIPLIERS:
+                suffix = f"lr={lr_multiplier:g}_samples={sample_size}_epochs={EPOCHS}_batch_size={BATCH_SIZE}"
+                logger.info(f"Creating finetuning job with {model}, {suffix=}...")
+                # Find if a job with these parameters already exists
+                exists, status, job_name = job_exists(
+                    existing_finetuning_jobs, model, file_id, lr_multiplier, BATCH_SIZE, EPOCHS
+                )
+                if exists:
+                    logger.info(f"Job already exists, {status=}, {job_name=}. Skipping")
+                    continue
+                hyperparameters = {
+                    "learning_rate_multiplier": lr_multiplier,
+                    "batch_size": BATCH_SIZE,
+                    "n_epochs": EPOCHS,
+                }
+                if input("Are you sure you want to send this job? (y/N)") != "y":
+                    logger.info("Skipping")
+                    continue
+                client.fine_tuning.jobs.create(
+                    model=model,
+                    training_file=file_id,
+                    seed=Config.seed,
+                    suffix=suffix,
+                    method={
+                        "type": "supervised",
+                        "supervised": {
+                            "hyperparameters": hyperparameters,
                         },
-                    )
-                    logger.info("Finetuning job created")
+                    },
+                )
+                logger.info("Finetuning job created")
 
 
 if __name__ == "__main__":
-    # main(gen_files=True, upload_files=True)
-    main(finetune=True)
+    # generate_files(upload=True)
+    finetune()
