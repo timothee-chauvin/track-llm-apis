5cf891fc478ca01bf781486bffed7ecc78ab8d67
Timothee Chauvin <timothee.chauvin28@gmail.com>
2025-08-20 18:50:14 +0200

    make logprob pvalue 5-15x faster with faster randperm and caching

--- src/track_llm_apis/sampling/analyze_logprobs.py
+++ src/track_llm_apis/sampling/analyze_logprobs.py
@@ -1,9 +1,57 @@
+from functools import cache
+
 import torch
+from frozendict import frozendict
+from frozenlist import FrozenList
 from jaxtyping import Float
 from torch import Tensor
 
 from track_llm_apis.config import config
 from track_llm_apis.sampling.common import OutputRow
+
+
+def frozenlist(li: list) -> FrozenList:
+    fl = FrozenList(li)
+    fl.freeze()
+    return fl
+
+
+@cache
+def build_logprob_tensor_row(
+    logprob: frozendict[int, float],
+    seen_tokens: frozenset[int],
+    token_short_ids: frozendict[int, int],
+) -> Float[Tensor, " nt"]:
+    t = torch.full(
+        (len(seen_tokens),), float("nan"), dtype=torch.float32, device=config.analysis.device
+    )
+    for token_id in seen_tokens:
+        if token_id in logprob:
+            t[token_short_ids[token_id]] = logprob[token_id]
+        else:
+            # Left censoring for the missing tokens
+            t[token_short_ids[token_id]] = min(logprob.values())
+    assert not torch.any(torch.isnan(t))
+    return t
+
+
+@cache
+def build_logprob_tensor(
+    logprobs: FrozenList[frozendict[int, float]],
+    seen_tokens: frozenset[int],
+    token_short_ids: frozendict[int, int],
+) -> Float[Tensor, "N nt"]:
+    t = torch.stack([build_logprob_tensor_row(lp, seen_tokens, token_short_ids) for lp in logprobs])
+    return t
+
+
+@cache
+def get_seen_tokens(
+    logprobs: FrozenList[frozendict[int, float]],
+) -> tuple[frozenset[int], frozendict[int, int]]:
+    seen_tokens = frozenset(token_id for lp in logprobs for token_id in lp.keys())
+    token_short_ids = frozendict({token_id: i for i, token_id in enumerate(seen_tokens)})
+    return seen_tokens, token_short_ids
 
 
 def logprob_two_sample_test(
@@ -12,33 +60,12 @@ def logprob_two_sample_test(
     b: int = 1000,
 ) -> tuple[float, float]:
     # Convert the samples to suitable tensors.
-    seen_tokens = set(
-        token_id for r in sample1 + sample2 for lp in r.logprobs for token_id in lp.keys()
-    )
-    token_short_ids = {token_id: i for i, token_id in enumerate(seen_tokens)}
-    t1 = torch.full(
-        (len(sample1), len(seen_tokens)),
-        float("nan"),
-        dtype=torch.float32,
-        device=config.analysis.device,
-    )
-    t2 = torch.full(
-        (len(sample2), len(seen_tokens)),
-        float("nan"),
-        dtype=torch.float32,
-        device=config.analysis.device,
-    )
-    for source, target in [(sample1, t1), (sample2, t2)]:
-        for i, row in enumerate(source):
-            assert len(row.logprobs) == 1
-            min_logprob = min(row.logprobs[0].values())
-            for token_id in seen_tokens:
-                if token_id in row.logprobs[0]:
-                    target[i, token_short_ids[token_id]] = row.logprobs[0][token_id]
-                else:
-                    # Left censoring for the missing tokens
-                    target[i, token_short_ids[token_id]] = min_logprob
-        assert not torch.any(torch.isnan(target))
+    all_logprobs = frozenlist([frozendict(r.logprobs[0]) for r in sample1 + sample2])
+    seen_tokens, token_short_ids = get_seen_tokens(all_logprobs)
+    sample1_logprobs = frozenlist([frozendict(r.logprobs[0]) for r in sample1])
+    sample2_logprobs = frozenlist([frozendict(r.logprobs[0]) for r in sample2])
+    t1 = build_logprob_tensor(sample1_logprobs, seen_tokens, token_short_ids)
+    t2 = build_logprob_tensor(sample2_logprobs, seen_tokens, token_short_ids)
     permutation_stats = logprob_two_sample_permutation_pvalue(t1, t2, b=b)
     statistic = logprob_two_sample_statistic(t1.unsqueeze(0), t2.unsqueeze(0)).item()
     pvalue = torch.mean(permutation_stats >= statistic, dim=0, dtype=torch.float32).item()
@@ -55,9 +82,8 @@ def logprob_two_sample_permutation_pvalue(
     N2 = t2.shape[0]
     nt = t1.shape[1]
     N_total = N1 + N2
-    perm_indices = torch.stack(
-        [torch.randperm(N_total, device=config.analysis.device) for _ in range(b)]
-    )
+    rand = torch.rand(b, N_total, device=config.analysis.device)
+    perm_indices = torch.argsort(rand, dim=1)
     permuted_samples = all_samples[perm_indices]
     assert permuted_samples.shape == (b, N_total, nt)
     permuted_t1 = permuted_samples[:, :N1, :]
