0c818a37f7ac6a599a2b46ce71ea81980991cbd5
Timothee Chauvin <timothee.chauvin28@gmail.com>
2025-10-22 15:08:33 +0200

    load_dotenv in __init__.py

--- src/track_llm_apis/__init__.py
+++ src/track_llm_apis/__init__.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
 from beartype.claw import beartype_this_package
+from dotenv import load_dotenv
 
 beartype_this_package()
+
+# TODO remove the load_dotenv calls in other files once there's a unified way to invoke code
+load_dotenv()
 
 
 def get_assets_dir() -> Path:
