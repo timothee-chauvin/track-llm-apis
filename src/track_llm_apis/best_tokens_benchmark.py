838483405fce52c5710e6ba001835c29f175aa15
Timothee Chauvin <timothee.chauvin28@gmail.com>
2025-12-16 15:15:27 +0100

    temperature: only float

--- src/track_llm_apis/best_tokens_benchmark.py
+++ src/track_llm_apis/best_tokens_benchmark.py
@@ -256,7 +256,7 @@ class BenchmarkEvaluator:
         }
 
 
-NAIVE_TEMPERATURES = [1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
+NAIVE_TEMPERATURES = [1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1.0]
 NAIVE_SAMPLES_PER_TOKEN = [5, 10, 20, 30, 60, 100, 200]
 
 
